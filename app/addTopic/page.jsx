591a0008a575fc75b36cffb6efc8c928a7a2b790
winnyDiese <winnydiese@gmail.com>
2023-10-20 22:11:45 +0200

    Finish to add Topic

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -1,12 +1,57 @@
 "use client"
 
+import { useState } from "react"
+import { useRouter } from "next/navigation"
+
 export default function AddTopics(){
-    console.log('hi')
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+
+    const router = useRouter()
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault()
+
+        if(!title || !description){
+            alert("title and description required")
+            return
+        }
+
+        try {
+            const res = await fetch('http://localhost:3000/api/topics',{
+                method:"POST",
+                headers: {
+                    "Content-type" : "application/json"
+                },
+                body: JSON.stringify({title, description})
+            })
+
+            if(res.ok) router.push('/')
+            else throw new Error("Failed to create a topic")
+
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
     return (
-        <form className="flex flex-col gap-3">
-            <input type="text" placeholder="Topic title" className="border border-slate-500 px-8 py-2" />
-            <input type="text" placeholder="Topic title" className="border border-slate-500 px-8 py-2" />
-            <button className="bg-green-600 font-bold text-white py-3 px-6 w-fit">Add Topic</button>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+            <input 
+                onChange={(e)=> setTitle(e.target.value)}
+                value={title}
+                type="text" 
+                placeholder="Topic title" 
+                className="border border-slate-500 px-8 py-2" 
+            />
+            <input 
+                onChange={(e)=> setDescription(e.target.value)}
+                value={description}
+                type="text" 
+                placeholder="Topic title" 
+                className="border border-slate-500 px-8 py-2" 
+            />
+            <button type="submit" className="bg-green-600 font-bold text-white py-3 px-6 w-fit">Add Topic</button>
         </form>
     )
 }
