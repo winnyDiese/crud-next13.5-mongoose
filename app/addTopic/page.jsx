597e77253e83df800fe11575d3aa000b6ee1f23d
winnyDiese <winnydiese@gmail.com>
2023-10-20 19:24:11 +0200

    Begin to connect BackEnd and FrontEnd, save part get data

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -1,5 +1,7 @@
+"use client"
 
 export default function AddTopics(){
+    console.log('hi')
     return (
         <form className="flex flex-col gap-3">
             <input type="text" placeholder="Topic title" className="border border-slate-500 px-8 py-2" />
--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -2,21 +2,39 @@ import Link from "next/link";
 import RemoveBtn from "./RemoveBtn";
 import HiPencilAlt, { HiPencil } from "react-icons/hi"
 
-export default function TopicsList(){
+const getTopics = async ()=>{
+    try{
+        const res = await fetch('http://localhost:3000/api/topics',{cache:"no-store"})
+        if(!res.ok){
+            throw new Error("Failed to fetch topics")
+        }
+        return res.json()
+    }catch(err){
+        console.log('Error loading topics: ', err)
+    }
+}
+
+
+export default async function TopicsList(){
+
+    const {topics} = await getTopics()
+
     return (
         <>
+            {topics.map( t => (
             <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
                 <div>
-                    <h2 className="font-bold text-2xl">Topic title</h2>
-                    <div>Topic description</div>
+                    <h2 className="font-bold text-2xl">{t.title} </h2>
+                    <div>{t.description} </div>
                 </div>
                 <div className="flex gap-2">
                     <RemoveBtn/>
-                    <Link href={"/editTopic/123"}>
+                    <Link href={`/editTopic/${t._id}`}>
                         <HiPencil size={24} />
                     </Link>
                 </div>
             </div>
+            ))}
         </>
     )
 }
