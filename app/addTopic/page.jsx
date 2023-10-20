e45ae30b49aee993f65127308f26eb865626f366
winnyDiese <winnydiese@gmail.com>
2023-10-20 17:08:40 +0200

    Finish List Topic, begin add Topic page

---
+++ app/addTopic/page.jsx
@@ -0,0 +1,8 @@
+
+export default function AddTopics(){
+    return (
+        <form className="flex flex-col gap-3">
+            <input type="text" placeholder="Topic title" className="border border-slate-500 px-8 py-2" />
+        </form>
+    )
+}
--- components/RemoveBtn.jsx
+++ components/RemoveBtn.jsx
@@ -1,6 +1,9 @@
+import { HiOutlineTrash } from "react-icons/hi";
 
 export default function RemoveBtn(){
     return (
-        <div>Remove btn</div>
+        <button className="text-red-400">
+            <HiOutlineTrash size={24} />
+        </button>
     )
 }
