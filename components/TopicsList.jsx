a96bb0ff43720360039be8f97b73e3f45368e371
winnyDiese <winnydiese@gmail.com>
2023-10-21 14:01:24 +0200

    Modif erreur in delete topic

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -22,13 +22,13 @@ export default async function TopicsList(){
     return (
         <>
             {topics.map( t => (
-            <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
+            <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start" key={t._id}>
                 <div>
                     <h2 className="font-bold text-2xl">{t.title} </h2>
                     <div>{t.description} </div>
                 </div>
                 <div className="flex gap-2">
-                    <RemoveBtn/>
+                    <RemoveBtn id={t._id} />
                     <Link href={`/editTopic/${t._id}`}>
                         <HiPencil size={24} />
                     </Link>
